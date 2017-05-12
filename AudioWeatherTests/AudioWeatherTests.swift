b7986e699e625c0b8405935888fad707a41b64c0
sweetchild222 <sweetchild22.ik@gmail.com>
2017-05-12 19:41:30 +0900

    enhance test code

--- AudioWeatherTests/AudioWeatherTests.swift
+++ AudioWeatherTests/AudioWeatherTests.swift
@@ -32,6 +32,33 @@ class AudioWeatherTests: XCTestCase {
     }
     
     
+    func testDFSConv(){
+        
+
+        let values = [[Double(60), Double(136), Double(37.995119577171813), Double(126.9958663037299)], [Double(97), Double(74), Double(35.101148844565955), Double(129.02478725562108)], [Double(53), Double(38), Double(33.500946412305076), Double(126.54663058817043)], [Double(43), Double(136), Double(38.00000000000003), Double(126)]]
+        
+        
+        for value in values{
+            
+            let gps = DFSConv.instance.toGPS(v1: value[0], v2: value[1])
+            let xy = DFSConv.instance.toXY(v1: gps[0], v2: gps[1])
+            
+            if gps[0] != value[2] || gps[1] != value[3]{
+                XCTAssert(false)
+            }
+            
+
+            if xy[0] != value[0] || xy[1] != value[1]{
+                XCTAssert(false)
+            }
+
+        }
+        
+    }
+    
+    
+    
+    /*
     func testWeatherRequest(){
         
         let expt = expectation(description: "Waiting weather request..")
@@ -53,9 +80,9 @@ class AudioWeatherTests: XCTestCase {
                 return
             }
         }
-        
-        
+ 
     }
+ */
     
     
     /*
