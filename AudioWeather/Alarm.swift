f53f2a6038328a8497afe5f6a269d74c9f4af6a0
sweetchild222 <sweetchild22.ik@gmail.com>
2017-06-01 22:27:32 +0900

    add alarm class

---
+++ AudioWeather/Alarm.swift
@@ -0,0 +1,16 @@
+//
+//  Alarm.swift
+//  AudioWeather
+//
+//  Created by 최인국 on 2017. 6. 1..
+//  Copyright © 2017년 최인국. All rights reserved.
+//
+
+import Foundation
+
+class Alarm{
+    
+    var date: Date = Date()
+    var enabled: Bool = false
+    var repeatWeekdays: [Int] = []    
+}
--- AudioWeather/AlarmTableViewController.swift
+++ AudioWeather/AlarmTableViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class AlarmTableViewController: UITableViewController {
+    
+    var check = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,7 +36,13 @@ class AlarmTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
+        
+        print("count")
+        if check == false{
         return 5
+        }
+        
+        return 4
     }
 
     
@@ -43,7 +51,6 @@ class AlarmTableViewController: UITableViewController {
 
         // Configure the cell...
         
-        
         cell.textLabel?.text = "gge"
         cell.detailTextLabel?.text = "232234"
         
@@ -51,25 +58,29 @@ class AlarmTableViewController: UITableViewController {
     }
     
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            
+            print(indexPath)
+            self.check = true
             tableView.deleteRows(at: [indexPath], with: .fade)
+            //self.check = true
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
