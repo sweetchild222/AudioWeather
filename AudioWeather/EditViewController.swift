5e1a30d8f6174f13352b1ba1db63b90e46fb9de7
sweetchild222 <sweetchild22.ik@gmail.com>
2017-06-03 19:30:27 +0900

    add ButtonWeek

--- AudioWeather/EditViewController.swift
+++ AudioWeather/EditViewController.swift
@@ -8,12 +8,19 @@
 
 import UIKit
 
-class EditViewController: UIViewController {
+class EditViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,16 +29,45 @@ class EditViewController: UIViewController {
     }
     
     @IBAction func pickDate(_ sender: Any) {
+    
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        
+        return 1000
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        //let cell = tableView.dequeueReusableCell(withIdentifier: Identifier.alarmCell, for: indexPath)
+        
+        let defaultIdentifer = "weekTableCell"
+        var cell = tableView.dequeueReusableCell(withIdentifier: defaultIdentifer)
+        if(cell == nil) {
+            cell = UITableViewCell(style: UITableViewCellStyle.value1, reuseIdentifier: defaultIdentifer)
+            print("-----")
+        }
+        else{
+            
+            print("okok")
+            
+        }
+        print(indexPath)
+
+        
+        //cell?.textLabel?.text = "gge"
+        //cell?.detailTextLabel?.text = "232234"
+        //cell?.accessoryType = UITableViewCellAccessoryType.disclosureIndicator
+        
+        return cell!
+    }
+    
+
 
 }
---
+++ AudioWeather/TableViewCellWeek.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCellWeek.swift
+//  AudioWeather
+//
+//  Created by 최인국 on 2017. 6. 3..
+//  Copyright © 2017년 최인국. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellWeek: UITableViewCell {
+
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
