eaee277c804812872463cfd50fb8d2f4098979ca
sweetchild222 <sweetchild22.ik@gmail.com>
2017-07-05 09:54:56 +0900

    adjust layout on dust cell

--- AudioWeather/CellDust.swift
+++ AudioWeather/CellDust.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class CellDust: UITableViewCell {
 
+    
+    @IBOutlet weak var pm25Image: UIImageView!
+    @IBOutlet weak var pm25Text: UILabel!
+    @IBOutlet weak var pm10Image: UIImageView!
+    @IBOutlet weak var pm10Text: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -28,13 +34,71 @@ class CellDust: UITableViewCell {
             return
         }
         
+        guard let value = AddressMap.instance.getMapInfo(addr:xAddress) else {
+            
+            return
+        }
+        
+        let area:String = value.getArea()
+        
+        guard let pm25Grade = dustList["PM25"]?[area] else {
+            
+            return
+        }
+        
+        guard let pm10Grade = dustList["PM10"]?[area] else {
+            
+            return
+        }
+
+
+        print(pm25Grade)
+        print(pm10Grade)
         
         
-        
+        self.pm25Text.text = pm25GradeToText(grade:pm25Grade)
+        self.pm10Text.text = pm10GradeToText(grade:pm10Grade)
         
         
         print("update dust\(xAddress.getText())")
         
+    }
+    
+    
+    
+    func pm25GradeToText(grade:DustRequester.Grade) -> String{
+        
+        switch grade {
+            
+        case .unknown:
+            return "미세먼지 모름"
+        case .good:
+            return "미세먼지 좋음"
+        case .normal:
+            return "미세먼지 보통"
+        case .bad:
+            return "미세먼지 나쁨"
+        case .worst:
+            return "미세먼지 매우나쁨"
+        }
+    }
+    
+    
+    func pm10GradeToText(grade:DustRequester.Grade) -> String{
+        
+        switch grade {
+            
+        case .unknown:
+            return "초미세먼지 모름"
+        case .good:
+            return "초미세먼지 좋음"
+        case .normal:
+            return "초미세먼지 보통"
+        case .bad:
+            return "초미세먼지 나쁨"
+        case .worst:
+            return "초미세먼지 매우나쁨"
+        }
     }
     
 }
